497df3e155f7cdaa83c30a026f7d9aac100e37a3
mixir <mixir@bk.ru>
2017-10-23 18:28:21 +0300

    *Перезалив проекта полный

--- app/src/main/java/com/httpso_hello/hello/activity/GuestsActivity.java
+++ app/src/main/java/com/httpso_hello/hello/activity/GuestsActivity.java
@@ -3,6 +3,7 @@ package com.httpso_hello.hello.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
@@ -30,55 +31,38 @@ public class GuestsActivity extends SuperMainActivity{
 
     private Profile profile;
     private GuestsListAdapter glAdapter;
-    private ProgressBar progressBarGuests;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        this.profile = new Profile(getApplicationContext());
-
         setContentView(R.layout.activity_guests);
+        setHeader();
 
-        progressBarGuests = (ProgressBar) findViewById(R.id.progressBarGuests);
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.refresh);
+        swipeRefreshLayout.setColorSchemeResources(
+                R.color.main_blue_color_hello,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+        swipeRefreshLayout.setRefreshing(true);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        getGuests();
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        //Во все активности перенести, заполнение шапки в меню
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-        View headerLayout = navigationView.getHeaderView(0);
-        headerLayout.setOnClickListener(new View.OnClickListener() {
+        // Свайп для обновления
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(GuestsActivity.this, ProfileActivity.class);
-                intent.putExtra("profile_id", stgs.getSettingInt("user_id"));
-                startActivity(intent);
-                finish();
+            public void onRefresh() {
+                swipeRefreshLayout.setRefreshing(true);
+                getGuests();
             }
         });
-        ImageView headerImageView = (ImageView) headerLayout.findViewById(R.id.user_avatar_header);
-        TextView user_name_and_age_header = (TextView) headerLayout.findViewById(R.id.user_name_and_age_header);
-        TextView user_id_header = (TextView) headerLayout.findViewById(R.id.user_id_header);
-        Picasso
-                .with(getApplicationContext())
-                .load(stgs.getSettingStr("user_avatar.micro"))
-                .resize(300, 300)
-                .centerCrop()
-                .transform(new CircularTransformation(0))
-                .into(headerImageView);
-        if(stgs.getSettingStr("user_age") != null) {
-            user_name_and_age_header.setText(stgs.getSettingStr("user_nickname") + ", " + stgs.getSettingStr("user_age"));
-        } else user_name_and_age_header.setText(stgs.getSettingStr("user_nickname"));
-        user_id_header.setText("Ваш ID " + Integer.toString(stgs.getSettingInt("user_id")));
+    }
 
+
+    private void getGuests() {
+        profile = new Profile(getApplicationContext());
         profile.getGuests(1, this, new Profile.GetGuestsCallback() {
             @Override
             public void onSuccess(final Guest[] guests, Activity activity) {
@@ -104,9 +88,7 @@ public class GuestsActivity extends SuperMainActivity{
                         startActivity(intent);
                     }
                 });
-
-                progressBarGuests.setVisibility(View.GONE);
-
+                swipeRefreshLayout.setRefreshing(false);
             }
 
             @Override
