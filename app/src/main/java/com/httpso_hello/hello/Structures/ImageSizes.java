14407c1f93f91c6a64e421f916f78795aceec591
Alexey <aleksey-kuvaev@mail.ru>
2017-12-04 23:03:12 +0300

    * Добавление размеров

---
+++ app/src/main/java/com/httpso_hello/hello/Structures/ImageSizes.java
@@ -0,0 +1,15 @@
+package com.httpso_hello.hello.Structures;
+
+/**
+ * Created by Общий on 04.12.2017.
+ */
+
+public class ImageSizes {
+
+    public Size original;
+    public Size normal_form_original;
+
+    public ImageSizes(){
+
+    }
+}
---
+++ app/src/main/java/com/httpso_hello/hello/Structures/Size.java
@@ -0,0 +1,14 @@
+package com.httpso_hello.hello.Structures;
+
+/**
+ * Created by Общий on 04.12.2017.
+ */
+
+public class Size {
+
+    public int width;
+    public int height;
+    public Size(){
+
+    }
+}
