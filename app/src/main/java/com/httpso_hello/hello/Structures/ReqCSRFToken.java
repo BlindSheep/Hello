1fa26b063b672e82ac0df1c98eb1bc3dad53329d
Alexey <aleksey-kuvaev@mail.ru>
2017-12-06 20:57:15 +0300

    * Функции для удаления групп

---
+++ app/src/main/java/com/httpso_hello/hello/Structures/ReqCSRFToken.java
@@ -0,0 +1,17 @@
+package com.httpso_hello.hello.Structures;
+
+import com.httpso_hello.hello.helper.RequestQ;
+
+/**
+ * Created by Общий on 06.12.2017.
+ */
+
+public class ReqCSRFToken {
+
+    public Error error;
+    public String csrf_token;
+
+    public ReqCSRFToken(){
+
+    }
+}
